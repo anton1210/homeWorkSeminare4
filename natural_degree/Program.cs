3ce4569a564bfadb4e829f8d5fde927b0021dba4
Anton Maslov <anton-maslov3@mail.ru>
2023-02-23 18:00:52 +0300

    Возведение в степень вариант 2

--- natural_degree/Program.cs
+++ natural_degree/Program.cs
@@ -2,13 +2,17 @@
 и возводит число A в натуральную степень B. Math.Pow использовать нельзя
 (3, 5 -> 243 (3⁵)   2, 4 -> 16) */
 
-void Degree(int A, int B)
+void Degree()
 {
+    Console.WriteLine("Введите число А: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень В: ");
+    int B = Convert.ToInt32(Console.ReadLine());
     int rez = 1;
     for (int i = 0; i < B; i++)
     {
         rez = A * rez;
     }
-    Console.WriteLine(rez);
+    Console.WriteLine($"Число А, в степени В = {rez}");
 }
-Degree(3, 5);
+Degree();
