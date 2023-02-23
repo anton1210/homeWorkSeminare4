b951fc24df0afeda51a7148c77a62ef935f6eed1
Anton Maslov <anton-maslov3@mail.ru>
2023-02-23 16:09:18 +0300

    Сумма введённых чисел

--- sum_digits_number/Program.cs
+++ sum_digits_number/Program.cs
@@ -1,2 +1,16 @@
 ﻿/* Задача 27: Напишите метод, который принимает на вход число 
 и выдаёт сумму цифр в числе. (452 -> 11     82 -> 10     9012 -> 12) */
+
+void Sum()
+{
+    Console.WriteLine("Введите число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = (num % 10) + sum;
+        num = num / 10;
+    }
+    Console.WriteLine(sum);
+}
+Sum();
