2d30d8bf4192e68df0fce8b8f580c49d2659c63f
Anton Maslov <anton-maslov3@mail.ru>
2023-02-24 09:50:24 +0300

    array

--- array_output/Program.cs
+++ array_output/Program.cs
@@ -1,3 +1,39 @@
 ﻿/* Задача 29: Напишите метод, который задаёт массив из N элементов 
 и выводит их на экран. (1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]  
 6, 1, 33 -> [6, 1, 33]) */
+
+
+int Print(string message)
+{
+    Console.Write(message);
+    string ReadInput = Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+// получения случайных чисел
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.Write("]");
+}
+
+int length = Print("введите длину массива: ");
+int min = Print("начальное значение диапазона: ");
+int max = Print("конечное значение диапазона: ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
